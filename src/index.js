8d3e8a0df8a21937fc900eb38b643e2016820895
jgretz <jgretz@truefitsolutions.com>
2017-03-03 12:31:59 -0500

    update for eslint

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
 import _ from 'lodash';
 import glob from 'glob';
 import path from 'path';
-import { logWarning } from 'node-bits';
+import {logWarning} from 'node-bits';
 
 // configuration
-const compileConfiguration = (options = {}, bitsConfig) => {
-  return {
+const compileConfiguration = (options = {}, bitsConfig) =>
+  ({
     root: '/',
     html: '/index.html',
 
     ...options,
     ...bitsConfig,
-  };
-};
+  });
 
 // routes
 const defineRoute = (configPath, filePath) => {
@@ -25,19 +24,19 @@ const defineRoute = (configPath, filePath) => {
     implementation: {
       get: (req, res) => {
         res.status(200).sendFile(filePath);
-      }
-    }
+      },
+    },
   };
 };
 
-export default (options) => {
-  return {
-    loadRoutes: (bitsConfig) => {
+export default options =>
+  ({
+    loadRoutes: bitsConfig => {
       const config = compileConfiguration(options, bitsConfig);
 
       // map all the files in the directory
       const files = glob.sync(path.join(config.path, '**/*.*'));
-      const routes = files.map((filePath) => defineRoute(config.path, filePath));
+      const routes = files.map(filePath => defineRoute(config.path, filePath));
 
       // map the index routes
       const index = _.find(routes, r => r.route === config.html);
@@ -47,10 +46,11 @@ export default (options) => {
         logWarning('No index html file found. Root route will not display.');
       }
 
-      _.forEach(roots, (root) => { routes.push({ ...index, route: root }); });
+      _.forEach(roots, root => {
+        routes.push({...index, route: root});
+      });
 
       // return
       return routes;
-    }
-  };
-};
+    },
+  });
